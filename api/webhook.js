5d2827ead82da50ac73395fee9914955412a6945
heitorpereira21 <hpff999@gmail.com>
2026-05-15 09:56:47 -0300

    fix 3

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,9 @@
-import { supabase } from '../lib/supabase'
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+)
 
 export default async function handler(req, res) {
   // 1. Bloqueia métodos que não sejam POST
